129a86505b29bed1ff4a63ceb455b1b2bd8b620e
ScottHarrisonDev <github@scottharrisondesign.co.uk>
2021-07-04 12:17:46 +0100

    fleshing out the Banner component

--- app/components/Banner.vue
+++ app/components/Banner.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   .bg-dark.py-3.px-10.flex.justify-center
-    button.text-sm.leading-5.text-beige-light.flex.justify-center.hover_border-beige-light.hover_border
+    button.text-sm.leading-5.text-beige-light.flex.justify-center.border-b.border-beige-light.border-opacity-0.hover_border-opacity-100.transition.ease-in-out.duration-500
       span Enjoy complimentary carbon neutral shipping on all orders.
       PlusIcon.ml-5(colour="beige-light" title="Test")
 </template>
