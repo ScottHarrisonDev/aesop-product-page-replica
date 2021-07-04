b169c74f322198cc91c48cccc566a1a9ebe425a9
ScottHarrisonDev <github@scottharrisondesign.co.uk>
2021-07-04 12:19:34 +0100

    set body background colour and text colour

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -8,6 +8,9 @@ export default {
     htmlAttrs: {
       lang: 'en'
     },
+    bodyAttrs: {
+      class: 'bg-dark text-beige-light'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
